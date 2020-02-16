71ba33cfa6cb7e8998bc3cd2706dc9a95884f33a
Chase McDonald <1chasemcd@gmail.com>
2020-02-16 08:49:15 -0800

    Add basic Strong Nuclear Force

--- public/script.js
+++ public/script.js
@@ -1,81 +1,72 @@
 // Prevent scroll
-function preventDefault(e)
-{
+function preventDefault(e) {
     e.preventDefault();
 }
 
-document.body.addEventListener('touchmove', preventDefault, { passive: false });
+document.body.addEventListener('touchmove', preventDefault,
+{ passive: false });
 
 
-// ----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 // Constants
-// ----------------------------------------------------------------------------
-const protonCharge = 1.6021766e-19; // C
-const protonMass = 1.6726219e-27; // kg
-const electronCharge = -1.6021766e-19; // C
-const electronMass = 9.10938356e-31; // kg
-const neutronMass = 1.674929e-27; // kg
+// ---------------------------------------------------------------------------
+const protonCharge = 1; // C
+const protonMass = 1000; // kg
+const electronCharge = -1; // C
+const electronMass = 1; // kg
+const protonRadius = 15;
+const electronRadius = 5;
+const snfDist = 15; // Distance at which strong nuclear force holds particles
 
-const k = 9.0e9; // Coulombs law constant
-
-const size = 5e-10; // size of space in pm
+const k = 1000; // Coulombs law constant
 
 const protonColor = '#5070ff';
 const neutronColor = '#20cccc';
 const electronColor = '#882020';
 
-// ----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 // Particle Superclass
-// ----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 
-class Particle
-{
-  constructor(pos)
-  {
+class Particle {
+  constructor(pos) {
     this.pos = createVector(pos.x, pos.y, pos.z);
     this.vel = createVector(0, 0, 0);
     this.acc = createVector(0, 0, 0);
   }
 
-  updateAcc()
-  {
-    var a = createVector(0, 0, 0);
-    for (var other of ps.particles)
-    {
-      if (other != this)
-      {
-        a.add(this.getAcc(other));
+  updateAcc() {
+      var a = createVector(0, 0, 0);
+      for (var other of ps.particles) {
+        if (other != this) {
+          a.add(this.getAcc(other));
+        }
       }
-    }
-    this.acc = a;
+      this.acc = a;
   }
 
-  updatePos()
-  {
+  updatePos() {
     this.vel.add(this.acc);
 
     this.pos.add(this.vel);
     var preLimitPos = createVector();
     preLimitPos.set(this.pos);
 
-    this.pos.limit(size / 2);
+    this.pos.limit(min(width, height) / 2);
 
-    if (!preLimitPos.equals(this.pos))
-    {
+    if (!preLimitPos.equals(this.pos)) {
       this.vel.set(0, 0, 0);
     }
   }
 
-  draw()
-  {
+  draw() {
     fill(this.color);
-    coordSphere(this.pos.x, this.pos.y, this.pos.z, 8);
+    coordSphere(this.pos.x, this.pos.y, this.pos.z, this.radius);
   }
 
   // Method to get acceleration based on Newton's 2nd law
   // F = ma
-  getAcc(other)
-  {
+  getAcc(other) {
     var force = this.getForce(other);
     var mag = force / this.mass;
 
@@ -88,65 +79,64 @@ class Particle
 
   // Method to get force applied to particle based on coulombs law
   // F = k|q1q2 / r^2|
-  getForce(other)
-  {
+  getForce(other) {
     var r = this.pos.dist(other.pos);
-    var force = (k * this.charge * other.charge) / (r*r);
+    var force = 0;
+    if (this.mass == protonMass && other.mass == protonMass) {
+      if (r > 2 * snfDist) {
+        force = (k * this.charge * other.charge) /
+          ((r - 2 * snfDist)*(r - 2 * snfDist));
+
+      } else {
+        force = -0.1 * k * Math.tan(Math.PI * (r - snfDist) / (2 * snfDist));
+      }
+    } else {
+      force = (k * this.charge * other.charge) / (r*r);
+    }
     return force;
   }
 }
 
-// ----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 // Subatomic Particles
-// ----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 
-class Proton extends Particle
-{
-  constructor(pos)
-  {
+class Proton extends Particle {
+  constructor(pos) {
     super(pos);
     this.color = color(protonColor);
     this.charge = protonCharge;
     this.mass = protonMass;
+    this.radius = protonRadius;
   }
 }
 
-class Neutron extends Particle
-{
-  constructor(pos)
-  {
+class Neutron extends Particle {
+  constructor(pos) {
     super(pos);
     this.color = color(neutronColor);
     this.charge = 0;
-    this.mass = neutronMass;
+    this.mass = protonMass;
+    this.radius = protonRadius;
   }
 }
 
-class Electron extends Particle
-{
-  constructor(pos)
-  {
+class Electron extends Particle {
+  constructor(pos) {
     super(pos);
     this.color = color(electronColor);
     this.charge = electronCharge;
     this.mass = electronMass;
-  }
-
-  draw()
-  {
-    fill(this.color);
-    coordSphere(this.pos.x, this.pos.y, this.pos.z, 2);
+    this.radius = electronRadius;
   }
 }
 
-// ----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 // GUI
-// ----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 
-class GUI
-{
-  constructor()
-  {
+class GUI {
+  constructor() {
     this.selected = 'pan';
     var ui = document.getElementById('gui').children;
     var left = ui[0].children;
@@ -160,27 +150,21 @@ class GUI
     this.buttons = {'pause': right[1], 'clear': right[0]};
   }
 
-  getClick(clicked)
-  {
-    if (clicked in this.radios)
-    {
+  getClick(clicked) {
+    if (clicked in this.radios) {
       this.radios[this.selected].classList.remove('selected');
       this.radios[clicked].classList.add('selected');
       this.selected = clicked;
-    }
-    else if (clicked in this.buttons)
-    {
-      if (clicked == 'clear')
-      {
+
+    } else if (clicked in this.buttons) {
+      if (clicked == 'clear') {
         ps.particles = [];
-      }
-      else if (clicked == 'pause')
-      {
-        if (ps.running)
-        {
+
+      } else if (clicked == 'pause') {
+        if (ps.running) {
           this.buttons['pause'].firstElementChild.src = 'icons/play.png';
-        }
-        else {
+
+        } else {
           this.buttons['pause'].firstElementChild.src = 'icons/pause.png';
         }
 
@@ -190,152 +174,128 @@ class GUI
   }
 }
 
-// ----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 // Mouse
-// ----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 
-class Mouse
-{
-  constructor()
-  {
+class Mouse {
+  constructor() {
     this.radius = 50;
-    this.pos3d = createVector();
-    this.pos2d = createVector();
+    this.pos = createVector();
     this.firstClick = null;
     this.prevAngle = createVector();
   }
 
-  update()
-  {
+  update() {
     var zyPos = createVector(0, mouseY - height/2);
     zyPos.rotate(-ps.cameraAngle.y);
 
     var xzPos = createVector(mouseX - width/2, zyPos.x);
     xzPos.rotate(ps.cameraAngle.x);
 
-    this.pos3d = pxTopm(createVector(xzPos.x, zyPos.y, xzPos.y));
-    this.pos2d = createVector(mouseX - width/2, mouseY - height/2);
+    this.pos.set(xzPos.x, zyPos.y, xzPos.y);
 
-    if (mouseIsPressed && gui.selected == 'move')
-    {
+    if (mouseIsPressed && gui.selected == 'move') {
       noFill();
       stroke(255);
       strokeWeight(3);
-      ellipse(this.pos2d.x, this.pos2d.y, this.radius*2, this.radius*2);
+      ellipse(mouseX - width/2, mouseY - height/2,
+        this.radius*2, this.radius*2);
       noStroke();
-    }
-    else if (mouseIsPressed && gui.selected == 'pan')
-    {
-      if (this.firstClick == null)
-      {
-        this.firstClick = createVector().set(this.pos2d);
+
+    } else if (mouseIsPressed && gui.selected == 'pan') {
+      if (this.firstClick == null) {
+        this.firstClick = createVector(mouseX, mouseY);
         this.prevAngle.set(ps.cameraAngle);
       }
 
       ps.cameraAngle.set((mouseX - this.firstClick.x) / 100,
         (mouseY - this.firstClick.y) / 100);
       ps.cameraAngle.add(this.prevAngle);
-    }
-    else if (!mouseIsPressed)
-    {
+    } else if (!mouseIsPressed) {
       this.firstClick = null;
     }
   }
 
-  inMouse(particle)
-  {
+  inMouse(particle) {
     return particle.pos.dist(new PVector(mX, mY)) <= radius &&
            mouseIsPressed && gui.selected == 'mouse';
   }
 
-  setPos()
-  {
+  setPos() {
     return new PVector(mouse - pmX, mY - pmY);
   }
 }
 
-// ----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 // Particle System
-// ----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 
-class ParticleSystem
-{
-  constructor()
-  {
+class ParticleSystem {
+  constructor() {
     this.particles = [];
     this.running = true;
     this.cameraAngle = createVector(0, 0);
     this.justPressed = false;
   }
 
-  update()
-  {
+  update() {
     rotateX(-this.cameraAngle.y);
     rotateY(this.cameraAngle.x);
-
     this.placeParticle();
 
-    if (this.running)
-    {
-      for (var particle of this.particles)
-      {
+    if (this.running) {
+      for (var particle of this.particles) {
         particle.updateAcc();
       }
 
-      for (var particle of this.particles)
-      {
+      for (var particle of this.particles) {
         particle.updatePos();
       }
     }
+
     this.draw();
   }
 
-  draw()
-  {
-    for (var particle of this.particles)
-    {
+  draw() {
+    for (var particle of this.particles) {
       particle.draw();
     }
   }
 
-  placeParticle()
-  {
+  placeParticle() {
     var inCircle = createVector(mouseX - width/2, mouseY - height/2).mag()
       < min(width, height) / 2;
-    if (mouseIsPressed && !this.justPressed && inCircle)
-    {
+
+    if (mouseIsPressed && !this.justPressed && inCircle) {
       this.justPressed = true;
-      if (gui.selected == 'proton')
-      {
+
+      if (gui.selected == 'proton') {
         this.particles.push(new Proton(mouse.pos));
-      }
-      else if (gui.selected == 'neutron')
-      {
+
+      } else if (gui.selected == 'neutron') {
         this.particles.push(new Neutron(mouse.pos));
-      }
-      else if (gui.selected == 'electron')
-      {
+
+      } else if (gui.selected == 'electron') {
         this.particles.push(new Electron(mouse.pos));
       }
-    }
-    else if (!mouseIsPressed)
-    {
+
+    } else if (!mouseIsPressed) {
       this.justPressed = false;
     }
   }
 }
 
-// ----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 // Manager
-// ----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 
 var ps;
 var canvas;
 var gui;
 var mouse;
 
-function setup()
-{
+function setup() {
   canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   canvas.parent('canvasholder');
   noStroke();
@@ -345,33 +305,20 @@ function setup()
   mouse = new Mouse();
 }
 
-function draw()
-{
+function draw() {
   background(0);
   lights();
   mouse.update();
   ps.update();
 }
 
-function windowResized()
-{
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function coordSphere(x, y, z, r)
-{
+function coordSphere(x, y, z, r) {
   push();
   translate(x, y, z);
   sphere(r);
   pop();
-}
-
-function pxTopm(pos)
-{
-  return p5.Vector.div(pos, min(width, height)).mult(size);
-}
-
-function pmTopx(pos)
-{
-  return p5.Vector.div(pos, size).mult(min(width, height));
 }
